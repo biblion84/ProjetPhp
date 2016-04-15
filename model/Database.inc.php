07758bfec16249f35bee374ca405ac2a26e1ea4a
blipn <oblipn@gmail.com>
2016-04-15 10:25:55 +0200

    MAJ DB

--- model/Database.inc.php
+++ model/Database.inc.php
@@ -40,29 +40,35 @@ class Database {
 	 *		title char(255),
 	 *		count integer);
 	 */
-	 private function createDataBase() {
-		 /* TODO START */
-		 $this->connection->exec("CREATE TABLE IF NOT EXISTS users (".
-		 " id INT NOT NULL AUTO_INCREMENT,". // ajout d'un id, c'est impératif
-		 " nickname varchar(20) NOT NULL,".
-		 " email varchar(30) NOT NULL,". // ajout d'un email !
-		 " password varchar(255) NOT NULL,". // 255 me parrait plus approprié pour un mdp
-		 " primary key ( id )".
-		 ");");
-		 $this->connection->exec("CREATE TABLE IF NOT EXISTS surveys (".
-		 " id INT NOT NULL AUTO_INCREMENT,".
-		 " owner varchar(20) NOT NULL,".
-		 " question varchar(255) NOT NULL,".
-		 " primary key ( id )".
-		 ");");
-		 $this->connection->exec("CREATE TABLE IF NOT EXISTS responses (".
-		 " id INT NOT NULL AUTO_INCREMENT,".
-		 " id_survey integer NOT NULL,".
-		 " title varchar(255) NOT NULL,".
-		 " count integer NOT NULL,".
-		 " primary key ( id ),".
-		 " FOREIGN KEY (id_survey) REFERENCES surveys(id)".
-		 ");");
+	 private function CreateDatabase() {
+	 	$this->connection->exec("CREATE TABLE IF NOT EXISTS users (".
+	 	"id INT NOT NULL AUTO_INCREMENT,".
+	 	"nickname VARCHAR(20) NOT NULL,".
+	 	"email VARCHAR(255) NOT NULL,".
+	 	"password VARCHAR(255) NOT NULL,".
+	 	"PRIMARY KEY(id)".
+	 	");");
+
+	 	$this->connection->exec("CREATE TABLE IF NOT EXISTS surveys (".
+	 	"id INT NOT NULL AUTO_INCREMENT,".
+	 	"owner_id INT NOT NULL,". // Jointure avec 'users'
+	 	"question VARCHAR(255) NOT NULL,".
+	 	"choices TEXT NOT NULL,". // Stockées sous la forme 'reponse1;reponse2;reponse3'
+	 	"responses TEXT NOT NULL,".  // Stockées sous la forme '45;25;68'
+	 	"PRIMARY KEY(id),".
+	  "FOREIGN KEY (owner_id) REFERENCES users(id)".
+	 	");");
+
+	 	$this->connection->exec("CREATE TABLE IF NOT EXISTS comments (".
+	 	"id INT NOT NULL AUTO_INCREMENT,".
+	 	"id_owner INT NOT NULL,". // Jointure avec 'users'
+	 	"id_survey INT NOT NULL,". // Jointure avec 'surveys'
+	 	"date DATE NOT NULL,".
+	 	"texte TEXT NOT NULL,".
+	 	"PRIMARY KEY(id),".
+	  "FOREIGN KEY (id_owner) REFERENCES users(id),".
+	  "FOREIGN KEY (id_survey) REFERENCES surveys(id)".
+	 	");");
 		/* TODO END */
 	}
 
