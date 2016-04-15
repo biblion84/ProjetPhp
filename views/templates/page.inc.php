fcbe4d83ee4ff558a2338ade2a94fb798e346859
blipn <oblipn@gmail.com>
2016-04-15 11:26:44 +0200

    MAJ bootstrap

--- views/templates/page.inc.php
+++ views/templates/page.inc.php
@@ -3,7 +3,7 @@
   <head>
 		<meta charset="utf-8">
 		<title>Sondages</title>
-		<link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="CSS/bootstrap.min.css" />
 </head>
 
 <body>
