8e2b7ad044317776c2ca0305f5626995ba5fdd25
blipn <oblipn@gmail.com>
2016-05-07 12:24:34 +0200

    L'enregistrement des votes + vérification d'ip fonctionne

--- actions/VoteAction.inc.php
+++ actions/VoteAction.inc.php
@@ -8,27 +8,29 @@ class VoteAction extends Action {
 	 * Récupère l'identifiant de la réponse choisie par l'utilisateur dans la variable
 	 * $_POST["responseId"] et met à jour le nombre de voix obtenues par cette réponse.
 	 * Pour ce faire, la méthode 'vote' de la classe 'Database' est utilisée.
-	 * 
+	 *
 	 * Si une erreur se produit, un message est affiché à l'utilisateur lui indiquant
 	 * que son vote n'a pas pu être pris en compte.
-	 * 
+	 *
 	 * Sinon, un message de confirmation lui est affiché.
 	 *
 	 * @see Action::run()
-	 */	
+	 */
 	public function run() {
-		
+
 		$responseId = "";
-		
+		$surveyId = "";
+
 		if (isset($_POST["responseId"])) $responseId = (int)trim($_POST["responseId"]);
-		
-		$r = $this->database->vote((int)$responseId);
-		
+		if (isset($_POST["surveyId"])) $surveyId = (int)trim($_POST["surveyId"]);
+
+		$r = $this->database->vote((int)$responseId, (int)$surveyId);
+
 		if ($r===false) {
-			$this->setMessageView("Impossible d'enregistrer votre vote.", "alert-error");
+			$this->setMessageView("Impossible d'enregistrer votre vote. Avez-vous déja voté ?", "alert-error");
 			return;
 		}
-		
+
 		$this->setMessageView("Votre vote a été enregistré.", "alert-success");
 	}
 
