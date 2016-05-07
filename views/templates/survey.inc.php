93113fc24a9135d2c77270b27fcc1a7ec0249f7d
blipn <oblipn@gmail.com>
2016-05-07 20:59:30 +0200

    css

--- views/templates/survey.inc.php
+++ views/templates/survey.inc.php
@@ -33,8 +33,12 @@
 					$comms = $survey->getComm();
 					for ($i=0; $i < count($survey->getComm()) ; $i++) {
 						echo "<blockquote><p>".$comms[$i]["texte"]."</p><footer style=\"font-size: 80%;color: #777;\">- écrit par ".$comms[$i]["nick_owner"]." le ".$comms[$i]["date"]." </footer></blockquote>";
-					}
-					?>
+					}?>
+					</div>
+					</li>
+					<li class="media well">
+					<div class="media-body">
+					<label class="sr-only" for="inputHelpBlock"><strong>Ajouter un Commentaire</strong></label>
 					<input type="text" class="span6" name="commentaire">
 					<input type="submit" class="btn" value="Commenter">
 				</form>
